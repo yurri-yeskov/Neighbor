832e677ad9c985d12d178d65772c45e4d5c707b8
Yurii Yeskov <83100719+yurri-yeskov@users.noreply.github.com>
2018-05-20 09:59:28 -0700

    feat: Add Foursquare API

--- src/App.js
+++ src/App.js
@@ -14,6 +14,33 @@ state = {
     locationClicked: {}
   }
 
+  foursquareAPI() {
+    // Foursquare API
+    const request = require('request');
+
+    request({
+      url: 'https://api.foursquare.com/v2/venues/explore',
+      method: 'GET',
+      qs: {
+        client_id: 'FTXP4WO54K05G1TYHCWIQYBH5OQRIG4SMSZBXYBV4MJWIZRT',
+        client_secret: 'C5HM10QZKYCGJZ2NILFVTSK2PF03C1WB0OIEC3CXX3KAAPDA',
+        ll: '34.1657053, -118.3970014',
+        query: 'golf',
+        v: '20180323',
+        limit: 15
+      }
+    }, function(err, res, body) {
+      if (err) {
+        console.error(err)
+      } else {
+        let obj = JSON.parse(body)
+        console.log(obj.response.groups["0"].items["0"].venue)
+
+      }
+    })
+    
+  }
+
   locationClick = (location) => {
     this.setState( (prevState) => {
       return { locationClicked: location}
@@ -21,6 +48,7 @@ state = {
   }
 
   render() {
+    this.foursquareAPI()
     return (
       <main>
         <Search 
